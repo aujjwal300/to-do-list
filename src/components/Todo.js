74f9a4395c0f7a151e4fc4ec2d011ea12c25708c
aujjwal300 <ujjwalagrawal300@gmail.com>
2022-05-29 20:27:29 +0530

    AddItem Component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,18 @@ import React, {useState} from 'react';
 
 const Todo = () => {
     const [inputData, setInputData] = useState("");
+    const [items, setItems] = useState([]);
+
+    const addItem = () => {
+        if (!inputData) {
+            alert("Plz fill the data!");
+        }
+        else {
+            setItems([inputData, ...items]);
+            setInputData("");
+        }
+    };
+
     return (
         <>
             <div className="main-div">
@@ -13,16 +25,22 @@ const Todo = () => {
                     <div className="addItems">
                         <input type="text" placeholder="✍ Add Item" className='form-control'
                             value={ inputData } onChange={(e) => setInputData(e.target.value) }/>
-                        <i className="fa fa-plus add-btn"/>
+                        <i className="fa fa-plus add-btn" onClick={addItem}/>
                     </div>
+
+                    {/* show our items */}
                     <div className="showItems">
-                        <div className="eachItem">
-                            <h3>Apple</h3>
-                            <div className="todo-btn">
-                                <i className="far fa-edit" aria-hidden="true"></i>
-                                <i className="far fa-trash-alt"></i>
-                            </div>
-                        </div>
+                        {items.map((currElem, index) => {
+                            return (
+                                <div className="eachItem">
+                                    <h3>{currElem}</h3>
+                                    <div className="todo-btn">
+                                        <i className="far fa-edit" aria-hidden="true"></i>
+                                        <i className="far fa-trash-alt"></i>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                     <div className="showItems">
                         <button className="btn effect04" data-sm-link-text="Remove All">
